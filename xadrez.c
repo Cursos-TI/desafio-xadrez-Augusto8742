d132de2682829e40bb3f6d7b947710d7b3dccbd8
Augusto8742 <augustodacruzoliveira@gmail.com>
2025-04-21 23:19:22 +0000

    xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 
+int main() {
 
-int main() {    
-        int i;
-      // Mover a torre 5 casas para a direita
-      for (int i = 0; i < 5; i++) {
-        printf("Direita\n"); // Imprimir a direção do movimento
-      }
-    
-      // Mover a torre 5 casas para a diagonal("cima,direita")
-        i = 0;
-      while (i < 5) {
-        printf("Cima,Direita\n"); // Imprimir a direção do movimento
-        i++;
-      }
-    
-      // Mover a torre 8 casas para a esquerda
-      i = 0;
-      do {
-        printf("Esquerda\n");
-        i++; }
-       while (i < 8); // Imprimir a direção do movimento
-    
+  int i;
+  int movimentoCompleto = 1; 
+
+// Mover a torre 5 casas para a direita
+for (int i = 0; i < 5; i++) {
+  printf("Direita\n"); // Imprimir a direção do movimento
+}
+
+// Mover a torre 5 casas para a diagonal("cima,direita")
+  i = 0;
+while (i < 5) {
+  printf("Cima,Direita\n"); // Imprimir a direção do movimento
+  i++;
+}
+
+// Mover a torre 8 casas para a esquerda
+i = 0;
+do {
+  printf("Esquerda\n");
+  i++; }
+while (i < 8); // Imprimir a direção do movimento
+
+while (movimentoCompleto--) // Movimento do cavalo "L"
+ {
+    for (int i =0; i < 2; i++) {
+        printf("Cima\n"); //imprimir "cima" duas vezes
+    }
+    printf("Direita\n"); //imprimir direita uma vez
+}
+
     return 0;
 }
